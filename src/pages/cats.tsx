27c49a79826ef8908d53c93a957927270c99c30f
JulianEhiem <jules.ehiem@gmail.com>
2022-12-02 15:35:09 -0500

    creating a test route

---
+++ src/pages/cats.tsx
@@ -0,0 +1,10 @@
+import React from 'preact/compat';
+
+export default function CatsPage() {
+    return(
+            <div>
+                <h2>This is a page for all the cats</h2>
+            </div>
+        )
+
+}
